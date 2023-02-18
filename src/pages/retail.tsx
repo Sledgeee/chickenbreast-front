0069834bfb380086c1a56148c9efece3b72a1523
Oleh Voloshyn <bazisc.qa@gmail.com>
2023-02-18 16:16:15 +0200

    fix aboslute url in ssr

--- src/pages/retail.tsx
+++ src/pages/retail.tsx
@@ -6,15 +6,15 @@ import { IRetail } from '@/components/pages/retail/retail.interface'
 import { ICategory } from '@/types/category.interface'
 import { IProduct } from '@/types/product.interface'
 
-import { API_URL } from '@/api/ky'
-
 const RetailPage: NextPage<IRetail> = ({ data }) => {
 	return <Retail data={data} />
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-	const products = await fetch(API_URL + '/products/')
-	const categories = await fetch(API_URL + '/category/')
+	const products = await fetch('https://chickenbreast-api.vercel.app/products/')
+	const categories = await fetch(
+		'https://chickenbreast-api.vercel.app/category/'
+	)
 
 	const prods = (await products.json()) as IProduct[]
 	const cats = (await categories.json()) as ICategory[]
